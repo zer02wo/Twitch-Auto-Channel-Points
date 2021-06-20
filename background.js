4a1b1e80e5ea97d28890f9de06084053dcaa9a3a
Stephen Stubbs <stephenstubbsbeta@gmail.com>
2021-06-20 12:20:14 +0100

    Moved background script to root folder

---
+++ background.js
@@ -0,0 +1,35 @@
+//TODO: https://developer.chrome.com/docs/extensions/mv3/background_pages/
+
+//Initialise extension on installation
+chrome.runtime.onInstalled.addListener(function(details) {
+    //TODO: figure out what actions need to be performed
+    if(details.reason == "install") {
+        //Perform some action
+    } else if(details.reason == "update") {
+        //Perform other action
+    }
+});
+
+//TODO: figure out what event to listen for to trigger starting the content script
+    //Might just be on page load, considering manifest only allows for www.twitch.tv urls
+
+//TODO: sets up communication with content script, this is what I'd use to update the storage (and probably the extension pop-up?) https://developer.chrome.com/docs/extensions/mv3/messaging/
+/*chrome.runtime.onConnect.addListener(function(port) {
+    //Writes error message to console if port doesn't match
+        //TODO: (change this to an if statement)?
+    console.assert(port.name === "channel-points");
+    //Sets up listener to port
+    port.onMessage.addListener(function(msg) {
+        if(msg.keyword == "Insert message here") {
+            port.postMessage({keyword: "Message received"})
+        }
+    });
+});*/
+
+//Create extension pop-up menu
+//TODO: chrome.browserAction.setPopup({popup: 'popup.html'});
+
+//TODO: Manifest comments:
+    //May not need scripting permission
+    //May need tabs permission to check all tabs (in theory should just run on each one?)
+    //Unsure if I can make storage permission optional and not cause errors
